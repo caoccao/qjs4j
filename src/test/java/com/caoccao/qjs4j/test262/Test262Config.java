ab338b8fd13d3a393ee1444f5bb949c58e5b47c1
Sam Cao <sjtucaocao@gmail.com>
2026-03-25 09:30:16 +0100

    refactor: Comment out maxTests assignment in Test262Config forLongRunningTest method

--- src/test/java/com/caoccao/qjs4j/test262/Test262Config.java
+++ src/test/java/com/caoccao/qjs4j/test262/Test262Config.java
@@ -55,7 +55,7 @@ public class Test262Config {
                 Pattern.compile(".*/test/built-ins/encodeURI.*/.*\\.js$"),
                 Pattern.compile(".*/test/staging/sm/Date/.*\\.js$"),
                 Pattern.compile(".*/test/built-ins/RegExp/.*\\.js$"));
-        config.maxTests = 600;
+        // config.maxTests = 600;
         return config;
     }
 
